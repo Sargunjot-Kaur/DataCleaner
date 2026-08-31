0adad4df92c6eda2926e09f1c743ef9c56bf9f39
Sargunjot Kaur <sargunxsandhu@icloud.com>
2026-08-31 14:02:25 +0200

    Analysing inconsistencies

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import pandas as pd
 
 #reads the csv file into a pandas DataFrame
-df = pd.read_csv('customer.csv')
+raw_df = pd.read_csv('customer.csv')
+clean_df = raw_df.copy()
 
-#number of columns
-print(df.shape[1]) 
-
-#names of the columns
-print(df.columns)
-
-#number of missing values per column
-print(df.isna().sum())
-
-#nube of duplicated rows
-print(df.duplicated().sum())
