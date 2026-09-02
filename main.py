42dd0de81b08e137ea438899f7f6cbbf45d530be
Sargunjot Kaur <sargunxsandhu@icloud.com>
2026-09-02 19:29:17 +0200

    Calculated numeric issues

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from profiler import count_duplicates, missing_vals, whitespace_changes, detect_numeric_columns
+from profiler import count_duplicates, missing_vals, whitespace_changes, detect_numeric_columns, count_numeric_issues
 from cleaner import strip_whitespace
+
 #reads the csv file into a pandas DataFrame
 raw_df = pd.read_csv('customer.csv')
 clean_df = raw_df.copy()
@@ -16,4 +17,7 @@ print("Missing values per column: ", missing_values)
 
 print("number of whitespaces found: ", whitespace_count)
 
-detect_numeric_columns(clean_df)
+numeric_columns=detect_numeric_columns(raw_df)
+print(numeric_columns)
+
+count_numeric_issues(raw_df, numeric_columns)
--- profiler.py
+++ profiler.py
@@ -14,13 +14,26 @@ def whitespace_changes(raw_df, clean_df):
         total += changes
     return total    
 
+numeric_columns = []
 def detect_numeric_columns (df):
     for column in df.columns:
         series = df[column]
         converted = pd.to_numeric(series, errors = "coerce")
+        
         ratio_numeric = converted.notna().sum() / series.notna().sum()
+        
         if ratio_numeric >= 0.8:
-                print (column, ratio_numeric)
+                numeric_columns.append(column)
 
+    return numeric_columns        
+
+def count_numeric_issues(df, numeric_columns):
+    for column in numeric_columns:
+         series = df[column]
+         converted = pd.to_numeric(series, errors = "coerce")
+         count= (series.notna() & converted.isna()).sum()
+
+         print (column, count)
     
+              
     
